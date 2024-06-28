7823fa263714cb53ccb46d0e15907d63241918db
brunomoraes97 <1997mbrunomoraes@gmail.com>
2024-06-28 14:19:17 -0300

    second commit

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import "fmt"
 
+// função que será chamada
 func olaMundo() {
-	fmt.Printf("Olá Mundo!")
+	fmt.Println("Olá Mundo!")
 }
 
+func dois() {
+	fmt.Println("Dois")
+}
+
+func tres() {
+	fmt.Println("Tres")
+}
+
+// função principal
+// entrypoint -> A função que será chamada
 func main() {
 	olaMundo()
+	dois()
+	tres()
 }
